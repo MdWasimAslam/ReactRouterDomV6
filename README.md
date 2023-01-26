564d6f5ae3b8e7428843ad213245bd4c3d0687d6
Md Wasim Aslam <wasimaslam2897@gmail.com>
2023-01-27 00:25:14 +0530

    #

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 `npm install`
 `npm start`
 
-### `npm start`
 
-[http://localhost:3000](http://localhost:3000) 
+
 ### `npm test`
 
 **Note: this is a one-way operation. Once you `eject`, you can't go back!**
--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes,Link, Navigate} from 'react-router-dom';
 import About from './Components/About';
 import Home from './Components/Home';
-
+import NavBar from './Components/NavBar';
+import './App.css'
+import ErrorPage from './Components/ErrorPage';
 
 function App() {
   return (
     
     <div>
       <BrowserRouter>
+      <NavBar/>
       <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/about' element={<About/>} />
+      
+      {/* This will render if path is not found in the routes usually an error page */}
+      <Route path='*' element={<ErrorPage />} />
+
+
+      {/* If Path is not avaiable then the below code will redirect it to Homepage using Navigate */}
+      {/* <Route path='*' element={<Navigate to={'/'} />} /> */}
+
       </Routes>
       </BrowserRouter>
     </div>
